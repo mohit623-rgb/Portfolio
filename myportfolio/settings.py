595f9d2a8854cd8ed2322b34433b1912e15f401d
Mohit <mdhiman2002186@gmail.com>
2025-12-31 13:48:25 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -79,14 +79,16 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'LFVrKnWRQJIdpKNnRUvZZsPqhJtcOFcl',
-        'HOST': 'mysql.railway.internal',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MYSQLDATABASE"),
+        "USER": os.environ.get("MYSQLUSER"),
+        "PASSWORD": os.environ.get("MYSQLPASSWORD"),
+        "HOST": os.environ.get("MYSQLHOST"),
+        "PORT": os.environ.get("MYSQLPORT", "3306"),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
 }
 
 
